5db89484ca7cd4cc78f421ddf94295e46b6ed040
shaheenppk <113391908+shaheenppk@users.noreply.github.com>
2022-09-20 15:38:30 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ if (existsSync('config.env')) require('dotenv').config({ path: './config.env' })
 const DATABASE_URL = process.env.DATABASE_URL === undefined ? './database.db' : process.env.DATABASE_URL
 module.exports = {
     VERSION: 'V 1.0.0',
-    SESSION_ID: process.env.SESSION_ID || '',
+    SESSION_ID: process.env.SESSION_ID || '',inrl~wPaHM865b77P7FSVUnDDwTeBhvDesPQ0
     PASSWORD: process.env.PASSWORD || '',
     U_STATUS: process.env.U_STATUS || 'true',
     MENTION: process.env.MENTION || 'on',
@@ -22,8 +22,8 @@ module.exports = {
     },
     profile: {
     ownerName: "inrl", 
-    ownerNumb: "917593919575", 
-    botName: "inrl-bot-md", 
+    ownerNumb: "923087609409", 
+    botName: "Shaheen Baloch", 
   },
     setting: {
     blockchat: [], // Your block chat Jids
@@ -49,12 +49,12 @@ module.exports = {
     IMG12 : "https://imgur.com/AelfUJg.jpg",
     ERRIMG : "https://imgur.com/ggvhL6C.jpg",
     PACKNAME : process.env.PACKNAME || "ɪɴʀʟ-ʙᴏᴛ",
-    GIT : process.env.GIT || "https://tinyurl.com/3ex3e48e",
-    WEB : process.env.WEB || "https://tinyurl.com/ycks3s8p",
-    YT : process.env.YT || "https://www.youtube.com/channel/null",
+    GIT : process.env.GIT || "https://youtu.be/DLFzQCZFfP0",
+    WEB : process.env.WEB || "https://youtu.be/DLFzQCZFfP0",
+    YT : process.env.YT || "https://youtu.be/DLFzQCZFfP0",
     CAPTION : process.env.CAPTION || "_created by inrl-bot_",
-    SUDO: process.env.SUDO || ['917593919575'],
+    SUDO: process.env.SUDO || ['923087609409'],
     VIDEO : "coming soon",
-    WAGRP : process.env.WAGRP || 'https://tinyurl.com/f5wh55mk',
+    WAGRP : process.env.WAGRP || 'https://youtu.be/DLFzQCZFfP0',
     DATABASE: DATABASE_URL === './database.db' ? new Sequelize({ dialect: 'sqlite', storage: DATABASE_URL, logging: false }) : new Sequelize(DATABASE_URL, {dialect: 'postgres', ssl: true, protocol: 'postgres', dialectOptions: { native: true, ssl: { require: true, rejectUnauthorized: false },}, logging: false }),
     };
